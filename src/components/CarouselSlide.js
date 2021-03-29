60c65c596566a5db2373329852afe9d109010a86
Stephen Tsim <41970987+tsimmerdown@users.noreply.github.com>
2021-03-29 15:53:08 -0400

    added projects

--- src/components/CarouselSlide.js
+++ src/components/CarouselSlide.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-
-import { Card } from '@material-ui/core';
-
+import { Card } from "@material-ui/core";
 
 const Cards = styled(Card)`
-    display: flex;
-    align-items: flex-end;
-    background-size: cover;
-    width: 80vh;
-    min-height: 40vh; 
-`
+  display: flex;
+  align-items: flex-end;
+  background-size: cover;
+  width: 80vh;
+  min-height: 40vh;
+`;
 
 const TextCont = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 5vh;
-    width: 100%;
-    background-color: #000000B3;
-    text-align: center;
-    color: white;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 5vh;
+  width: 100%;
+  background-color: #000000b3;
+  text-align: center;
+  color: white;
+`;
 
-const CarouselSlide = ({content}) => {
-
-    return (
-        <Cards style={{backgroundImage: content.backgroundImage}}>
-           <TextCont>{content.title}</TextCont>
+const CarouselSlide = ({ content }) => {
+  return (
+    <>
+      <a href={content.link}>
+        <Cards style={{ backgroundImage: content.backgroundImage }}>
+          <TextCont>{content.title}</TextCont>
         </Cards>
-    );
-}
+      </a>
+    </>
+  );
+};
 export default CarouselSlide;
--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,21 @@
 export const SLIDE_INFO = [
-    { backgroundImage: 'url(./Images/capstone.PNG)', title: 'ROS Application UI' },
-    { backgroundImage: 'url(./Images/cansim.PNG)', title: 'Can Sim Health Canada Application' },
+  {
+    backgroundImage: "url(./Images/sweetbundle.jpg)",
+    title: "Sweet Bundle by Ying",
+    link: "https://github.com/tsimmerdown/SweetBundle",
+  },
+  {
+    backgroundImage: "url(./Images/foodforthought.jpg)",
+    title: "Food For Thought",
+    link: "https://github.com/tsimmerdown/FoodForThought",
+  },
+  {
+    backgroundImage: "url(./Images/capstone.PNG)",
+    title: "ROS Application UI",
+  },
+  {
+    backgroundImage: "url(./Images/cansim.PNG)",
+    title: "Can Sim Health Canada Application",
+    link: "http://can-sim.ca/hc/",
+  },
 ];
